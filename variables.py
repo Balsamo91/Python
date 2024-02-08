29340b7e3915dbab597e98765e19a92a6bb0c6a5
Valerio <valerio.balsamini@gmail.com>
2024-02-08 19:09:00 +0000

    Lesson 03 Python Hello World

---
+++ variables.py
@@ -0,0 +1,27 @@
+# This is sigle line comment
+
+"""
+This is a multi line comment
+hello, another line
+"""
+
+# This is a single line string
+message = "Hello World!" 
+
+# This is a multi line string
+message_multi = '''Hey! 
+This is a new line!
+'''
+# This is an Intiger
+number = 8
+
+# This is a Float
+weight_kg = 4.58
+
+# This is a Boolean
+is_raining = True
+
+# This is None
+this_is_empty = None
+
+print(message_multi)
